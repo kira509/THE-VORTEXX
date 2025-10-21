a869b0d69ba341fde3cba548a50aefae4424d186
kira509 <tacetygre87@gmail.com>
2025-10-21 20:53:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // ✅ using SWC instead of old plugin
 
 export default defineConfig({
   plugins: [react()],
